95f9d22cf22451bcb129fad762674f9d25140c89
Anish Das <anishservice12@gmail.com>
2023-07-07 12:22:55 +0530

    v1.0

---
+++ script.js
@@ -0,0 +1,31 @@
+let input = document.getElementById('input')
+let btn = document.getElementById('btn')
+let text = document.getElementById('text')
+btn.addEventListener('click', work)
+input.addEventListener('keypress',function(e){
+   if (e.key == "Enter")
+    {
+      work();   
+    }
+})
+function work()
+{
+if(input.value == "")
+{
+    alert("Enter a value")
+}
+else
+{
+let newele = document.createElement("h3");
+newele.innerHTML = `${input.value}<i class="fa-solid fa-trash fa-shake"></i>`;
+text.appendChild(newele);
+input.value = "";
+let rem = newele.querySelector("i");
+rem.addEventListener("click",del);
+function del()
+{
+    newele.remove();
+}
+
+}
+}
